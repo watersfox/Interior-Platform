c3cb07f98a231871b7225adbc4c6f3d367eaed93
watersfox <qnfrhcwjswod@naver.com>
2023-11-22 11:51:36 +0900

    pt connectTest 파일 주석 추가, 반복문 100 -> 50으로 수정

--- pt/src/testpackage/connectTest.java
+++ pt/src/testpackage/connectTest.java
@@ -1,10 +1,10 @@
 package testpackage;
 
-//연동테스트
+//연동테스트1
 public class connectTest {
 	public static void main(String[] args){
 		
-		for(int i=0; i<100; i++) {
+		for(int i=0; i<50; i++) {
 			if(i%2==0)
 			System.out.println(i+1+"% 연동");
 		}
