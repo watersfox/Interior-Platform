5a1755e877c10a237592a9edd0a9d5f64ab80031
bsw0828 <bsw0828@naver.com>
2024-01-26 11:15:27 +0900

    add

---
+++ src/main/java/com/interiormon/interiorProject/domain/Comment.java
@@ -0,0 +1,34 @@
+package com.interiormon.interiorProject.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+
+    @Id
+    private int commentNumber;
+    
+    private String userId;
+    @ManyToOne
+    private User user;
+    
+    private int postNumber;
+    private String content;
+    private int level;
+    private int bundleId;
+    private int bundleOrder;
+    private boolean isDeleted;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    private LocalDateTime deletedDate;
+    private int likesCnt;
+}
+
