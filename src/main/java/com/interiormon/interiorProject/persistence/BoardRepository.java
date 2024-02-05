39d0920c4677248073e0a067b0c7355a4d38803a
ielo97 <myc7188@naver.com>
2024-02-05 13:48:21 +0900

    delete

--- src/main/java/com/interiormon/interiorProject/persistence/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.interiormon.interiorProject.persistence;
-
-import com.interiormon.interiorProject.domain.CommunityPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<CommunityPost, Integer> {
-}
